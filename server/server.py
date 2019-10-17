11394b2a33e48584d8ed4981316fb6f2206238c9
Rico Klimpel <rico@tklimpel.de>
2019-10-17 16:13:24 +0200

    add simple server argument processing

--- server/server.py
+++ server/server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask,escape,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Microstation User!'
+def hello_microstation():
+    name = request.args.get("name","")
+    if(name is ""):
+        return "hello unkown microstation user."
+    else:
+        return 'hello, ' + name + '. \nwelcome at microstation server.'
+    
