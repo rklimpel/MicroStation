a7b7240a27aabb42d148635247c11b3ea0071ad0
Rico Klimpel <rico@tklimpel.de>
2019-10-20 17:22:26 +0200

    read arguments from serial commands and removed unnecealksjdfl readmes

--- station/wifi-controller/src/main.cpp
+++ station/wifi-controller/src/main.cpp
@@ -1,18 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-void testConnection();
-void establishConnection();
+void testConnection(String name);
+void establishConnection(String ssid,String pw);
 void executeCommand(String cmd);
+String getArgument(String argId,String cmd);
  
 const char* ssid = "FRITZ!Box 7362 SL";
 const char* password = "96716995986792895394";
  
 void setup () {
- 
   Serial.begin(115200);
-  establishConnection();
- 
+  establishConnection(ssid,password);
 }
  
 void loop() {
@@ -24,23 +23,37 @@ void loop() {
 }
 
 void executeCommand(String cmd){
-    if(cmd.indexOf("$TEST") != -1){
-        testConnection();
-    }else if(cmd.indexOf("$CONNECT") != -1){
-        establishConnection();
+    if(cmd.indexOf("TEST") != -1){
+        String name = getArgument(cmd,"NAME");
+        testConnection(name);
+    }else if(cmd.indexOf("CONNECT") != -1){
+        String ssid = getArgument(cmd,"SSID");
+        String pw = getArgument(cmd,"PW");
+        establishConnection(ssid,pw);
     }
 }
 
-void establishConnection(){
+String getArgument(String cmd,String argId){
+    String arg = "&"+argId+"=";
+    int argStart = cmd.indexOf(arg)+arg.length();
+    int argEnd = cmd.indexOf("&",argStart) > 0 ? cmd.indexOf("&",argStart) : cmd.indexOf("$",argStart);
+    return cmd.substring(argStart,argEnd);
+}
+
+void establishConnection(String ssid, String password){
     WiFi.begin(ssid, password);
    
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.println("Connecting...");
     }
+
+    Serial.println("Connected to " + (String)ssid + ".");
 }
 
-void testConnection(){
+void testConnection(String name){
+
+    Serial.println("name="+name);
 
     if (WiFi.status() == WL_CONNECTED) {
 
@@ -49,7 +62,7 @@ void testConnection(){
         WiFiClient wifiClient;
         HTTPClient http;
         
-        http.begin(wifiClient,"http://192.168.178.195:5000");
+        http.begin(wifiClient,"http://192.168.178.195:5000/?name="+name);
         int httpCode = http.GET();
         
         if (httpCode > 0) {
