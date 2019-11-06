0ce03632a832e9bdf08a8a4a95b46ee00d2b401f
Rico Klimpel <rico@tklimpel.de>
2019-11-07 00:18:15 +0100

    updated esp8266 controller code, wifi and print and request improvements

--- station/wifi-controller/src/main.cpp
+++ station/wifi-controller/src/main.cpp
@@ -5,12 +5,14 @@
 
 void establishConnection(String ssid,String pw);
 void executeCommand(String cmd);
+void printAvailableNetworks();
+const char* wifiStatusToString(wl_status_t status);
 String getArgument(String argId,String cmd);
 String sendPOSTRequest(String url, String body);
 String sendGETRequest(String url);
  
-const char* ssid = "FRITZ!Box 7362 SL";
-const char* password = "96716995986792895394";
+const char* ssid = "Zum Einhornland";
+const char* password = "Butterbreadcoffee23";
  
 void setup () {
   Serial.begin(SERIAL_BOUND_RATE);
@@ -38,6 +40,8 @@ void executeCommand(String cmd){
         String url = getArgument(cmd,"URL");
         String body = getArgument(cmd,"BODY");
         sendPOSTRequest(url,body);
+    }else if(cmd.indexOf("§PRINT-NETWORKS") != -1){
+        printAvailableNetworks();
     }
 }
 
@@ -73,25 +77,66 @@ String sendPOSTRequest(String url, String body){
         WiFiClient wifiClient;
         HTTPClient http;
         http.begin(wifiClient,url);
-        http.addHeader("Content-Type", "text/plain");
+        http.addHeader("content-type", "application/json");
         int httpCode = http.POST(body);  
         if (httpCode > 0) {
             response = http.getString();
         }else{
             response = "ERROR";
         }
+        Serial.println("Got return code " + String(httpCode));
+        Serial.println("Response: " + response);
         http.end();
     }
     return response;
 }
 
-void establishConnection(String ssid, String password){
-    WiFi.begin(ssid, password);
-   
-    while (WiFi.status() != WL_CONNECTED) {
+void establishConnection(String newssid, String newpassword){
+    Serial.println("Connect to '"+newssid+"' with pw '"+newpassword+"'");
+
+    WiFi.begin(newssid, newpassword);
+
+    Serial.print("Connecting to ");
+    Serial.print(newssid); 
+    Serial.println(" ...");
+
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
         delay(1000);
-        Serial.println("Connecting...");
+        Serial.print(++i); Serial.print(' ');
     }
 
-    Serial.println("Connected to " + (String)ssid + ".");
+    Serial.println('\n');
+    Serial.println("Connection established!");  
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.SSID());
+    Serial.println(WiFi.localIP()); 
+}
+
+void printAvailableNetworks(){
+
+    int numberOfNetworks = WiFi.scanNetworks();
+ 
+    for(int i =0; i<numberOfNetworks; i++){
+ 
+        Serial.print("Network name: ");
+        Serial.println(WiFi.SSID(i));
+        Serial.print("Signal strength: ");
+        Serial.println(WiFi.RSSI(i));
+        Serial.println("-----------------------");
+ 
+    }
+}
+
+const char* wifiStatusToString(wl_status_t status) {
+  switch (status) {
+    case WL_NO_SHIELD: return "WL_NO_SHIELD";
+    case WL_IDLE_STATUS: return "WL_IDLE_STATUS";
+    case WL_NO_SSID_AVAIL: return "WL_NO_SSID_AVAIL";
+    case WL_SCAN_COMPLETED: return "WL_SCAN_COMPLETED";
+    case WL_CONNECTED: return "WL_CONNECTED";
+    case WL_CONNECT_FAILED: return "WL_CONNECT_FAILED";
+    case WL_CONNECTION_LOST: return "WL_CONNECTION_LOST";
+    case WL_DISCONNECTED: return "WL_DISCONNECTED";
+  }
 }
